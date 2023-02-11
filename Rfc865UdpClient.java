8371f9cdd126585d951f33718c2aafc6003f64a6
Soh Zu Wei <laserklet@gmail.com>
2023-02-11 22:52:41 +0800

    socket programming in java

---
+++ Rfc865UdpClient.java
@@ -0,0 +1,44 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.net.DatagramPacket;
+import java.io.IOException;
+import java.net.InetAddress;
+
+
+public class Rfc865UdpClient {
+    static DatagramSocket socket;
+    
+    
+
+    public static void main(String[] argv) throws UnknownHostException {
+        //
+        // 1. Open UDP socket
+        //
+        String hostname = "localhost";
+        int port = 17;
+        InetAddress myAddress = InetAddress.getByName(hostname);
+
+
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {}
+        try {
+            //
+            // 2. Send UDP request to server
+            //
+            byte[] buffer = new byte[512];
+
+            DatagramPacket request = new DatagramPacket(buffer, buffer.length, myAddress, port);
+            socket.send(request);
+            
+            //
+            // 3. Receive UDP reply from server
+            //
+            DatagramPacket reply = new DatagramPacket(buffer, 0, request.getLength());
+            socket.receive(reply);
+            
+            System.out.println(new String(buffer, 0, reply.getLength()));
+        } catch (IOException e) {}
+    }
+}
---
+++ Rfc865UdpServer.java
@@ -0,0 +1,45 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.DatagramPacket;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Rfc865UdpServer {
+
+    static DatagramSocket socket;
+
+    public static void main(String[] argv) {
+        //
+        // 1. Open UDP socket at well-known port
+        //
+        
+        try {
+            socket = new DatagramSocket(17);
+        } 
+        catch (SocketException e) {}
+        
+        while (true) {
+            try {
+                //
+                // 2. Listen for UDP request from client
+                //
+                byte[] buffer = new byte[256];
+
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                socket.receive(request);
+                
+                //
+                // 3. Send UDP reply to client
+                //
+                InetAddress clientAddress = request.getAddress();
+                int clientPort = request.getPort();
+                String quote = "With great power, comes great responsibility";
+                buffer = quote.getBytes();
+
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
+                System.out.printf("Sending reply to {%s} {%s}", clientAddress, clientPort);
+                socket.send(reply);
+            } catch (IOException e) {}
+        }
+    }
+}
